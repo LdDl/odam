842d1d96d87e708eb66f8446891769d9a63a33a6
Dimitrii Lopanov <sexykdi@gmail.com>
2021-08-27 19:50:42 +0300

    check if polygon is convex or concave

--- virtual_polygons.go
+++ virtual_polygons.go
@@ -26,3 +26,56 @@ type VirtualPolygon struct {
 	// Type of virtual polygon: could be convex or concave
 	PolygonType POLYGON_TYPE `json:"-"`
 }
+
+// Constructor for VirtualPolygon
+// (x1, y1) - Left
+// (x2, y2) - Right
+func NewVirtualPolygon(pairs ...[2]int) *VirtualPolygon {
+	vpolygon := VirtualPolygon{
+		Coordinates:       make([]image.Point, len(pairs)),
+		SourceCoordinates: make([]image.Point, len(pairs)),
+	}
+	for i := range pairs {
+		vpolygon.Coordinates[i] = image.Point{X: pairs[i][0], Y: pairs[i][1]}
+		vpolygon.SourceCoordinates[i] = image.Point{X: pairs[i][0], Y: pairs[i][1]}
+	}
+	if vpolygon.isConvex() {
+		vpolygon.PolygonType = CONVEX_POLYGON
+	} else {
+		vpolygon.PolygonType = NOT_CONVEX_POLYGON
+	}
+	return &vpolygon
+}
+
+func (vpolygon *VirtualPolygon) isConvex() bool {
+	n := len(vpolygon.Coordinates)
+	if n < 3 {
+		// Well, this is not that strange if polygon have been prepared wrongly
+		return false
+	}
+	previousCrossProduct := 0
+	currentCrossProduct := 0
+	for i := range vpolygon.Coordinates {
+		currentCrossProduct = crossProduct(vpolygon.Coordinates[i], vpolygon.Coordinates[(i+1)%n], vpolygon.Coordinates[(i+2)%n])
+		if currentCrossProduct != 0 {
+			if currentCrossProduct*previousCrossProduct < 0 {
+				return false
+			} else {
+				previousCrossProduct = currentCrossProduct
+			}
+		}
+	}
+	return true
+}
+
+func crossProduct(a image.Point, b image.Point, c image.Point) int {
+	// direction of vector b.x -> a.x
+	x1 := b.X - a.X
+	// direction of vector b.y -> a.y
+	y1 := b.Y - a.Y
+	// direction of vector c.x -> a.x
+	x2 := c.X - a.X
+	// direction of vector c.y -> a.y
+	y2 := c.Y - a.Y
+	return x1*y2 + y1*x2
+}
