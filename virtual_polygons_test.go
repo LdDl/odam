22c205721c0cf86343d0a7cbdc6eb85dc882c82d
Dimitrii Lopanov <sexykdi@gmail.com>
2021-08-28 18:25:34 +0300

    test polygon enter/left/inside/outside for second test: true

--- virtual_polygons_test.go
+++ virtual_polygons_test.go
@@ -184,9 +184,9 @@ func TestPolygonBlobEnter(t *testing.T) {
 		}
 	}
 
-	simpleDTime0 := blob.NewSimpleBlobie(image.Rect(49, 16, 64, 23), nil)
-	simpleDTime1 := blob.NewSimpleBlobie(image.Rect(48, 20, 63, 27), nil)
-	simpleDTime2 := blob.NewSimpleBlobie(image.Rect(48, 25, 63, 33), nil)
+	simpleDTime0 := blob.NewSimpleBlobie(image.Rect(10, 9, 25, 18), nil)
+	simpleDTime1 := blob.NewSimpleBlobie(image.Rect(12, 16, 27, 24), nil)
+	simpleDTime2 := blob.NewSimpleBlobie(image.Rect(11, 21, 27, 30), nil)
 	allblobiesOutside := blob.NewBlobiesDefaults()
 	allblobiesOutside.MatchToExisting([]blob.Blobie{simpleDTime0, simpleDTime1, simpleDTime2})
 	for _, b := range allblobiesOutside.Objects {
@@ -198,5 +198,59 @@ func TestPolygonBlobEnter(t *testing.T) {
 }
 
 func TestPolygonBlobLeft(t *testing.T) {
-	// @todo
+	/* Using same data as in TestPolygonBlobEnter() */
+
+	simpleATime0 := blob.NewSimpleBlobie(image.Rect(30, 2, 43, 13), nil)
+	simpleATime1 := blob.NewSimpleBlobie(image.Rect(28, 8, 41, 18), nil)
+	simpleATime2 := blob.NewSimpleBlobie(image.Rect(29, 17, 43, 26), nil)
+	allblobiesEnter := blob.NewBlobiesDefaults()
+	allblobiesEnter.MatchToExisting([]blob.Blobie{simpleATime0, simpleATime1, simpleATime2})
+	vpolygon := NewVirtualPolygon(
+		image.Point{X: 23, Y: 15},
+		image.Point{X: 67, Y: 15},
+		image.Point{X: 67, Y: 41},
+		image.Point{X: 23, Y: 41},
+	)
+	for _, b := range allblobiesEnter.Objects {
+		center := b.GetCenter()
+		if vpolygon.BlobLeft(b) {
+			t.Errorf("Blob with center at [%d, %d] should has NOT been left polygon with coordinates %v", center.X, center.Y, vpolygon.Coordinates)
+		}
+	}
+
+	simpleBTime0 := blob.NewSimpleBlobie(image.Rect(38, 32, 52, 39), nil)
+	simpleBTime1 := blob.NewSimpleBlobie(image.Rect(40, 35, 53, 42), nil)
+	simpleBTime2 := blob.NewSimpleBlobie(image.Rect(42, 42, 56, 50), nil)
+	allblobiesLeft := blob.NewBlobiesDefaults()
+	allblobiesLeft.MatchToExisting([]blob.Blobie{simpleBTime0, simpleBTime1, simpleBTime2})
+	for _, b := range allblobiesLeft.Objects {
+		center := b.GetCenter()
+		if !vpolygon.BlobLeft(b) {
+			t.Errorf("Blob with center at [%d, %d] should has been left polygon with coordinates %v", center.X, center.Y, vpolygon.Coordinates)
+		}
+	}
+
+	simpleCTime0 := blob.NewSimpleBlobie(image.Rect(49, 16, 64, 23), nil)
+	simpleCTime1 := blob.NewSimpleBlobie(image.Rect(48, 20, 63, 27), nil)
+	simpleCTime2 := blob.NewSimpleBlobie(image.Rect(48, 25, 63, 33), nil)
+	allblobiesInside := blob.NewBlobiesDefaults()
+	allblobiesInside.MatchToExisting([]blob.Blobie{simpleCTime0, simpleCTime1, simpleCTime2})
+	for _, b := range allblobiesInside.Objects {
+		center := b.GetCenter()
+		if vpolygon.BlobLeft(b) {
+			t.Errorf("Blob with center at [%d, %d] should has NOT been left polygon with coordinates %v", center.X, center.Y, vpolygon.Coordinates)
+		}
+	}
+
+	simpleDTime0 := blob.NewSimpleBlobie(image.Rect(10, 9, 25, 18), nil)
+	simpleDTime1 := blob.NewSimpleBlobie(image.Rect(12, 16, 27, 24), nil)
+	simpleDTime2 := blob.NewSimpleBlobie(image.Rect(11, 21, 27, 30), nil)
+	allblobiesOutside := blob.NewBlobiesDefaults()
+	allblobiesOutside.MatchToExisting([]blob.Blobie{simpleDTime0, simpleDTime1, simpleDTime2})
+	for _, b := range allblobiesOutside.Objects {
+		center := b.GetCenter()
+		if vpolygon.BlobLeft(b) {
+			t.Errorf("Blob with center at [%d, %d] should has NOT been left polygon with coordinates %v", center.X, center.Y, vpolygon.Coordinates)
+		}
+	}
 }
